998e5256e0de15761eb32cd73246b16524b99de4
TekfolioNG <tekfoilong@gmail.com>
2025-12-22 00:26:13 +0100

    Fix Sanity config for production

--- .history/nuxt.config_20251222002437.ts
+++ .history/nuxt.config_20251222002437.ts
@@ -84,6 +84,7 @@ export default defineNuxtConfig({
       failOnError: false,
       crawlLinks: false,
       routes: ['/'],
+      ignore: ['/blog', '/blog/**'] 
     },
     cloudflare: {
       pages: {
